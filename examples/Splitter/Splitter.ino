f836ab4419e5270bba4ace629ae22711351ac506
AlexBeem <71223577+AlexBeem@users.noreply.github.com>
2021-11-26 15:25:16 +0300

    Update Splitter.ino

--- examples/Splitter/Splitter.ino
+++ examples/Splitter/Splitter.ino
@@ -3,17 +3,17 @@
 #define P_RX 2                        // Reception PIN (SoftSerial)
 #define P_TX 5                        // Transmition PIN (SoftSerial)
 
-SoftSerial Bluetooth(P_RX, P_TX);     // Software serial port for control the Bluetooth module
+SoftSerial iBUS(P_RX, P_TX);     // Software serial port for control the Bluetooth module
 
 void setup()
 {
-    Bluetooth.begin(115200); // Initialize the serial port
+  iBUS.begin(115200); // Initialize the serial port
   Serial.begin(115200);
 }
 
 void loop()
 {
     // Basic Echo Test
-    if(Bluetooth.available())
-    Serial.write(Bluetooth.read());
+    if(iBUS.available())
+    Serial.write(iBUS.read());
 }
